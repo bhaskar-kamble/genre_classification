2f9dd54b8d873b7ba197c988833ba81f1ff87062
bhaskar-kamble <kbhaskar.iitk@gmail.com>
2025-04-22 11:56:50 +0200

    compared with provided solution

--- main.py
+++ main.py
@@ -44,10 +44,10 @@ def go(config: DictConfig):
             os.path.join(root_path, "preprocess"),
             "main",
             parameters={
-                "input_artifact":"raw_data.parquet",
+                "input_artifact":"raw_data.parquet:latest", # REMEMBER TO USE THE VERSION!!
                 "artifact_name":"preprocessed_data.csv", # choosing this name as config["data"]["reference_dataset"] is called this
                 "artifact_type":"preprocessed_data", # this is something i gave
-                "artifact_description":"this is the processed data"
+                "artifact_description":"Data with processing applied"
             }
         )
 
@@ -71,11 +71,13 @@ def go(config: DictConfig):
             os.path.join(root_path, "segregate"),
             "main",
             parameters={
-                "input_artifact":config["data"]["reference_dataset"],
+                #"input_artifact":config["data"]["reference_dataset"],
+                "input_artifact":"preprocessed_data.csv:latest",#(better to explicitly chain this to the output artifact of the "preprocess" component)
                 "artifact_root":"data",
                 "artifact_type":"segregated_data",#this is something i gave
                 "test_size":config["data"]["test_size"],
-                "random_state":config["main"]["random_seed"],#or config["random_forest_pipeline"]["random_forest"]["random_state"]?
+                #"random_state":config["main"]["random_seed"],#or config["random_forest_pipeline"]["random_forest"]["random_state"]?
+                #commenting out above, as required=False and default value provided in /segregate/run.py
                 "stratify":config["data"]["stratify"]
             }
         )
@@ -93,11 +95,14 @@ def go(config: DictConfig):
             os.path.join(root_path, "random_forest"),
             "main",
             parameters={
-                "train_data":[],
+                "train_data":"data_train.csv:latest",
                 "model_config":model_config,
                 "export_artifact":config["random_forest_pipeline"]["export_artifact"],
+                #if you don't want to export the inference artifact, set the above to "null".
                 "random_seed":config["main"]["random_seed"],#or config["random_forest_pipeline"]["random_forest"]["random_state"]?
-                "val_size":config["data"]["val_size"],
+                "val_size":config["data"]["val_size"],# but the solution says config["data"]["test_size"]
+                # what's the point of giving both config["data"]["val_size"] and config["data"]["test_size"]?
+                # the "evaluate" component will anyway check on the entire data_test.csv artifact created earlier. 
                 "stratify":config["data"]["stratify"]
             }
         )
@@ -109,8 +114,11 @@ def go(config: DictConfig):
             os.path.join(root_path, "evaluate"),
             "main",
             parameters={
-                "model_export":config["random_forest_pipeline"]["export_artifact"],
-                "test_data":"data_test.csv/latest"
+                #"model_export":config["random_forest_pipeline"]["export_artifact"],
+                #you cannot set latest tag above, so do it as follows:
+                "model_export": f"{config['random_forest_pipeline']['export_artifact']}:latest",
+                # or like this: config['random_forest_pipeline']['export_artifact']+":latest"
+                "test_data":"data_test.csv:latest"
             }
         )
 
